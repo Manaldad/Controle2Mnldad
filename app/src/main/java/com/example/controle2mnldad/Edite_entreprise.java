2efcc6181c6ea94b752521d5912623aef4bb7cb6
medohamss <moussamohamadyassine@gmail.com>
2022-06-02 16:55:16 +0100

    Q11)-a)-b)-c)-d)

--- app/src/main/java/com/example/controle2mnldad/Edite_entreprise.java
+++ app/src/main/java/com/example/controle2mnldad/Edite_entreprise.java
@@ -1,14 +1,55 @@
 package com.example.controle2mnldad;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class Edite_entreprise extends AppCompatActivity {
+    Spinner sp;
+    MyDatabase db;
+    EditText e1,e2,e3;
+    ArrayList<Entreprise> entrps;
+    ArrayAdapter ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edite_entreprise);
+        db=new MyDatabase(getApplicationContext());
+        sp=findViewById(R.id.sp);
+        e1=findViewById(R.id.e1Edit);
+        e2=findViewById(R.id.e2Edit);
+        e3=findViewById(R.id.e3Edit);
+        entrps=MyDatabase.getAllEntreprise(db.getReadableDatabase());
+        ArrayList<String> id=new ArrayList<>();
+        for(Entreprise e:entrps)
+            id.add(String.valueOf(e.getID()));
+        ad=new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,id);
+        sp.setAdapter(ad);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Entreprise e=entrps.get(i);
+                e1.setText(e.getRaisonSociale());
+                e2.setText(e.getAdresse());
+                e3.setText(String.valueOf(e.getCapitale()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
-}
+
+    }
+
+
